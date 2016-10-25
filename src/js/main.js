81dca867f07414d301ccb5a47f1cda54bcfc1d25
Christos <christos_zenonos@hotmail.com>
2016-10-25 11:31:21 +0100

    adding Dropdown box

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,17 @@ request.send(null);
 var jsonPlayersObject = JSON.parse(request.responseText);
 var playersObject = jsonPlayersObject.players;
 
-var namesArray = [];
-
 for (var key in playersObject){
     if (playersObject.hasOwnProperty(key)) {
-        console.log(playersObject[key]);
-        namesArray.push(playersObject[key].player.name)
+        var playerName = playersObject[key].player.name.first + ' ' +playersObject[key].player.name.last
+
+        populateSelectBox('player', key, playerName);
     }
+}
+
+
+function populateSelectBox(selector, key, value)
+{
+    var select = document.getElementById(selector);
+        select.options[select.options.length] = new Option(value, key);
 }
