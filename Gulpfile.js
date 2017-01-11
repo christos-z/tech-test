f3afe3215ff6390da642c55dc0e6b00b32b4b39e
Christos <christos_zenonos@hotmail.com>
2017-01-11 23:08:25 +0000

    initial commit, bootstraping jquery, bootstrap3 and gulp runner to watch for sass changes and compile css

---
+++ Gulpfile.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Christos on 11/01/2017.
+ */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var input = 'assets/stylesheets/scss/**/*.scss';
+var output = 'assets/stylesheets/css';
+
+var sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+};
+
+gulp.task('sass', function () {
+    return gulp
+        .src(input)
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(gulp.dest(output));
+});
+
+gulp.task('watch', function() {
+    return gulp
+        .watch(input, ['sass'])
+        .on('change', function(event) {
+            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        });
+});
+
+gulp.task('default', ['sass', 'watch']);
