04af528709071ce4e73110f96e36a20f26dd4704
Christos <christos_zenonos@hotmail.com>
2017-01-15 21:29:48 +0000

    Final commit. Removed some unnecessary files. Separated JS and CSS logic. Ensured page is W3C valid. Added logic to allow slider to work with touch slide movements also.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,46 @@
 /**
  * Created by Christos on 13/01/2017.
  */
-$('#collapseExample').on('shown.bs.collapse', function() {
-    console.log("shown");
-}).on('show.bs.collapse', function() {
-    console.log("show");
+function toggleHiddenBannerText() {
+    $(".show-more-info__content").on('show.bs.collapse', function(){
+        $(".promotion-cover > h3").toggleClass("hidden")
+    }).on('hide.bs.collapse', function(){
+        $(".promotion-cover > h3").toggleClass("hidden")
+    });
+}
+
+function toggleActiveCtaBanner() {
+    $(".cta-banner").on('click', function() {
+        if ($(this).hasClass('active')) {
+            $(".cta-banner").removeClass('active');
+        }
+        else {
+            $(".cta-banner").removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+}
+
+function carouselSlideMobile() {
+    $("#cta-buttons-carousel").on("touchstart", function(event){
+        var xClick = event.originalEvent.touches[0].pageX;
+        $(this).one("touchmove", function(event){
+            var xMove = event.originalEvent.touches[0].pageX;
+            if( Math.floor(xClick - xMove) > 5 ){
+                $("#cta-buttons-carousel").carousel('next');
+            }
+            else if( Math.floor(xClick - xMove) < -5 ){
+                $("#cta-buttons-carousel").carousel('prev');
+            }
+        });
+        $("#cta-buttons-carousel").on("touchend", function(){
+            $(this).off("touchmove");
+        });
+    });
+}
+
+$(document).ready(function(){
+    toggleHiddenBannerText();
+    toggleActiveCtaBanner();
+    carouselSlideMobile();
 });
